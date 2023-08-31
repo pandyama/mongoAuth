f413d456768b3e63e1e0874e5c0f84c60432bd6e
pandyama <pandyama2@gmail.com>
2023-08-30 22:51:39 -0600

    test log

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = require("express")();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+const UserModel = require("./model/user");
+
 mongoose
   .connect("mongodb://localhost/myapp", {
     useNewUrlParser: true,
@@ -22,14 +24,14 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", UserSchema);
-
 app.post("/register", async (req, res, next) => {
   // here, you can get the username and password from req.body in the actual site. Here, we will hardcode the values.
   const username = "sample2";
   const password = "password";
   const hashedPw = await bcrypt.hash(password, 12);
-  const user = await User.create({ username, hashedPw });
+
+  // const user = await User.create({ username, hashedPw });
+  const user = new UserModel({ username, password: hashedPw });
   await user.save();
   return res.send(user);
 });
---
+++ util/constants.js
@@ -0,0 +1,3 @@
+exports.schema = Object.freeze({
+  USER: "User",
+});
